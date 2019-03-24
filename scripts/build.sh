138f11d8fc320352d8d579e97e2f05f630639437
Андрей Романов <romanov_andrey_vl@protonmail.com>
2019-03-24 14:35:27 +0300

    подготовка к докладу завершена

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,13 +8,18 @@ echo "Начинаем сборку docker-compose..."
 docker-compose -f scripts/docker-compose.yml build --no-cache
 docker-compose -f scripts/docker-compose.yml up -d
 
+echo ""
 echo "Адрес Cassandra: " "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' cassandra-service)"
 echo "Адрес Kafka: " "$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kafka-service)"
 echo "Эти адреса добавляете в application.yml для настройки Kafka и Cassandra"
 echo ""
 
-echo "Перед запуском: нужно подключиться к Cassandra, и выполнить вот такую команду: "
-echo "CREATE KEYSPACE IF NOT EXISTS demo WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};"
+echo "Важно: для подключения к Cassandra нужно создать keyspace".
+echo "Команды для этого: "
+echo "docker exec -it cassandra-service bash"
+echo "cqlsh *ip контейнера с Cassandra*"
+echo "CREATE KEYSPACE IF NOT EXISTS reactivekeyspace WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};"
+
 echo ""
 
 echo "Как собрать producer: cd reactivchina-producer && ./gradlew clean bootJar"
